28ba1866b4229a453dcf26e62562498fbe7bd678
CASPARBOETES <CASPARBOETES@GMAIL.COM>
2018-02-08 15:39:28 +0100

    add sessions, users, auth, jwt token

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-const bodyParser = require('body-parser')
+// index.js
 const express = require('express')
-const { recipes } = require('./routes')
-
+const bodyParser = require('body-parser')
+const passport = require('./config/auth')
+const { recipes, users, sessions } = require('./routes')
 
 const port = process.env.PORT || 3030
 
 let app = express()
 
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
+app
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use(bodyParser.json())
+  .use(passport.initialize())
+  .use(recipes)
+  .use(users)
+  .use(sessions)
 
-.use(recipes)
-
-.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
-
-.use((err, req, res, next) => {
-  res.status(err.status || 500)
-  res.send({
-    message: err.message,
-    // only print full errors in development
-    error: app.get('env') === 'development' ? err : {}
+  // catch 404 and forward to error handler
+  .use((req, res, next) => {
+    const err = new Error('Not Found')
+    err.status = 404
+    next(err)
   })
-})
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+  // final error handler
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send({
+      message: err.message,
+      error: app.get('env') === 'development' ? err : {}
+    })
+  })
+
+  .listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+  })
--- routes/recipes.js
+++ routes/recipes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router()
 const { Recipe } = require('../models')
 
-app.get('/recipes', (req, res, next) => {
+router.get('/recipes', (req, res, next) => {
   Recipe.find()
   .sort({ createdAt: -1 }) // Newest recipe first
   .then((recipes) => res.json(recipes)) // Send data in JSON format
   .catch((error) => next(error)) // Forward any errors to error handler
-})
+  })
 
-app.get('/recipes/:id', (req, res, next) => {
+  .get('/recipes/:id', (req, res, next) => {
   const id = req.params.id
   Recipe.findById(id)
     .then((recipe) => {
@@ -16,25 +16,25 @@ app.get('/recipes/:id', (req, res, next) => {
       res.json(recipe)
     })
     .catch((error) => next(error))
-})
+  })
 
-app.post('/recipes',  (req, res, next) => {
+  .post('/recipes',  (req, res, next) => {
   let newRecipe = req.body
   Recipe.create(newRecipe)
     .then((recipe) => res.json(recipe))
     .catch((error) => next(error))
-})
+  })
 
-app.put('/recipes/:id',  (req, res, next) => {
+  .put('/recipes/:id',  (req, res, next) => {
   const recipeId = req.params.id
   let update = req.body
 
   Recipe.findOneAndUpdate(recipeId, update)
     .then((recipe) => res.json(recipe))
     .catch((error) => next(error))
-})
+  })
 
-app.patch('/recipes/:id',  (req, res, next) => {
+  .patch('/recipes/:id',  (req, res, next) => {
   const recipeId = req.params.id
   let update = req.body
 
@@ -42,14 +42,14 @@ app.patch('/recipes/:id',  (req, res, next) => {
   Recipe.findOneAndUpdate(recipeId, update)
     .then((recipe) => res.json(recipe))
     .catch((error) => next(error))
-})
+  })
 
-app.delete('/recipes/:id',  (req, res, next) => {
+  .delete('/recipes/:id',  (req, res, next) => {
   const recipeId = req.params.id
 
   Recipe.findOneAndRemove(recipeId)
     .then((recipe) => res.json(recipe))
     .catch((error) => next(error))
-})
+  })
 
-module.export = router
+module.exports = router
